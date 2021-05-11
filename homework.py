db4c594e60b3ac6da297d826d10310b3b4faf253
Zverev Dmitriy <zverev_armstrong@inbox.ru>
2021-05-11 21:20:21 +0300

    Part 3.2 added

--- homework.py
+++ homework.py
@@ -16,18 +16,6 @@ class Student:
         else:
             return 'Ошибка'
 
-    def mid_score(self, grades_dict):
-        total_score = 0
-        total_number = 0
-        for key in grades_dict.keys():
-            for elem in grades_dict[key]:
-                total_score += elem
-                total_number += 1
-        if total_number:
-            return total_score / total_number
-        else:
-            return "Оценок не обнаружено"
-
     def list_process(self, list_needed):
         if len(list_needed):
             result_str = list_needed[0]
@@ -40,10 +28,13 @@ class Student:
     def __str__(self):
         result = (f'Имя: {self.name}\n'
                   f'Фамилия: {self.surname}\n'
-                  f'Средняя оценка за домашние задания: {self.mid_score(self.grades)}\n'
+                  f'Средняя оценка за домашние задания: {mid_score(self.grades)}\n'
                   f'Курсы в процессе изучения: {self.list_process(self.courses_in_progress)}\n'
                   f'Завершенные курсы: {self.list_process(self.finished_courses)}\n')
         return result
+
+    def __lt__(self, other_student):
+        return compare(self, other_student, Student)
 
 
 class Mentor:
@@ -61,8 +52,11 @@ class Lecturer(Mentor):
     def __str__(self):
         result = (f'Имя: {self.name}\n'
                   f'Фамилия: {self.surname}\n'
-                  f'Средняя оценка за лекции {Student.mid_score(self,self.grades)}\n')
+                  f'Средняя оценка за лекции {mid_score(self.grades)}\n')
         return result
+
+    def __lt__(self, other_lecturer):
+        return compare(self, other_lecturer, Lecturer)
 
 
 class Reviewer(Mentor):
@@ -84,25 +78,91 @@ class Reviewer(Mentor):
         return result
 
 
+def mid_score(grades_dict):
+    total_score = 0
+    total_number = 0
+    for key in grades_dict.keys():
+        for elem in grades_dict[key]:
+            total_score += elem
+            total_number += 1
+    if total_number:
+        return total_score / total_number
+    else:
+        return "Оценок не обнаружено"
+
+
+def compare(class1, class2, root_class):
+    if isinstance(class2, root_class):
+        candidate1 = f'{class1.name} {class1.surname}'
+        candidate2 = f'{class2.name} {class2.surname}'
+        if mid_score(class1.grades) > mid_score(class2.grades):
+            line = f'{candidate1} имеет средний балл больше чем {candidate2}'
+        elif mid_score(class1.grades) == mid_score(class2.grades):
+            line = f'{candidate1} и {candidate2} имеют одинаковый балл'
+        else:
+            line = f'{candidate2} имеет средний балл больше чем {candidate1}'
+        return line
+    else:
+        err_line = f'Некорректный ввод все данный должны быть {root_class}'
+        return err_line
+
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['BIM']
 
+mid_student = Student('Emmely', 'Riot', 'your_gender')
+mid_student.courses_in_progress += ['Python']
+mid_student.courses_in_progress += ['BIM']
+mid_student.courses_in_progress += ['XOR']
+
 mentor_reviewer = Reviewer('Some', 'Buddy Revier')
-mentor_lecturer = Lecturer('Boddy', 'Some Lector')
 mentor_reviewer.courses_attached += ['Python']
+
+mentor2_reviewer = Reviewer('Dummy', 'Buer')
+mentor2_reviewer.courses_attached += ['BIM']
+
+mentor_lecturer = Lecturer('Boddy', 'Some Lector')
 mentor_lecturer.courses_attached += ['Python']
+
+mentor2_lecturer = Lecturer('BoomBuddy', 'Furious')
+mentor2_lecturer.courses_attached += ['Python']
+mentor2_lecturer.courses_attached += ['BIM']
 
 mentor_reviewer.rate_hw(best_student, 'Python', 10)
 mentor_reviewer.rate_hw(best_student, 'Python', 9)
-mentor_reviewer.rate_hw(best_student, 'BIM', 8)
-mentor_reviewer.rate_hw(best_student, 'BIM', 6)
-best_student.rate_lector(mentor_lecturer, 'Python', 8)
-best_student.rate_lector(mentor_lecturer, 'Pythonize', 6)
-best_student.rate_lector(mentor_reviewer, 'Pythonize', 0)
+mentor2_reviewer.rate_hw(best_student, 'BIM', 8)
+mentor2_reviewer.rate_hw(best_student, 'BIM', 6)
+mentor_reviewer.rate_hw(mid_student, 'Python', 7)
+mentor_reviewer.rate_hw(mid_student, 'Python', 3)
+mentor2_reviewer.rate_hw(mid_student, 'BIM', 2)
+mentor2_reviewer.rate_hw(mid_student, 'BIM', 10)
 
-# print(best_student.grades)
-# print(mentor_lecturer.grades)
+best_student.rate_lector(mentor_lecturer, 'Python', 8)
+best_student.rate_lector(mentor2_lecturer, 'Python', 8)
+best_student.rate_lector(mentor2_lecturer, 'BIM', 9)
+
+mid_student.rate_lector(mentor_lecturer, 'Python', 2)
+mid_student.rate_lector(mentor2_lecturer, 'Python', 3)
+mid_student.rate_lector(mentor2_lecturer, 'BIM', 5)
+
+print(best_student.grades)
+print(mid_student.grades)
+print('________________')
+print(mentor_lecturer.grades)
+print(mentor2_lecturer.grades)
+print('________________')
 print(best_student)
+print(mid_student)
+print('________________')
 print(mentor_lecturer)
+print(mentor2_lecturer)
+print('________________')
 print(mentor_reviewer)
+print('________________')
+print(mentor2_lecturer < mentor_lecturer)
+print(mentor2_lecturer < best_student)
+print('________________')
+print(best_student < mentor2_lecturer)
+print(best_student < mid_student)
+
